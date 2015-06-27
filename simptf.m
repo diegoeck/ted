29fa06e4fb2111bc1cdb95784856ea65534012f4
Diego Eckhard <diego@eckhard.com.br>
2015-06-27 10:48:07 -0300

    corrige erros para zeros e polos imaginários

--- simptf.m
+++ simptf.m
@@ -1,11 +1,19 @@
-function [G]=simtf(G);
+function [G]=simptf(G);
 %This function simplify a transfer function G
 %
-%Usage: [G]=simtf(G)
+%Usage: [G]=simptf(G)
 %
-%Diego Eckhard - 20/09/2011
+%Diego Eckhard - 25/06/2014
 %UFRGS Identification Toolbox
-[z,p,k,t]=zpkdata(G,'v');
+t=size(G);
+k1=t(1);
+k2=t(2);
+for ii=1:k1
+for jj=1:k2
+
+    
+[z,p,k,t]=zpkdata(G(ii,jj),'v');
+
 
 a=0;
 while a==0
@@ -16,8 +24,35 @@ while a==0
         for j=1:length(z)
 
             if round(1000*p(i))==round(1000*z(j))
+                
+                
+                polo=p(i);
+                zero=z(j);
+                
                 p(i)=[];
                 z(j)=[];
+                
+             
+                
+                if imag(polo)>10^-4
+                    p(i)=[];
+                    z(j)=[];
+%                     for iii=i:length(p)
+%                        if round(1000*polo')==round(1000*p(iii))
+%                         p(iii)=[];
+%                         
+%                        end
+%                       break
+%                     end
+%                     for jjj=j:length(z)
+%                        if round(1000*zero')==round(1000*z(jjj))
+%                         z(jjj)=[];
+%                        end
+%                        break
+%                     end
+%                 
+                end
+                
                 a=0;
                 break
             end
@@ -28,4 +63,27 @@ while a==0
 
     end
 end
-G=zpk(z,p,k,t);
+
+for i=1:length(p)
+if abs(imag(p(i)))<10^-4
+    p(i)=real(p(i));
+end
+end
+
+for i=1:length(z)
+if abs(imag(z(i)))<10^-4
+    z(i)=real(z(i));
+end
+end
+
+if abs(k)<10^-4
+    k=0
+end
+
+G(ii,jj)=zpk(z,p,k,t);
+
+
+
+
+end
+end
